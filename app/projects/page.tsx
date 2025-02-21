09804e283b7bc48e92629f4ef03149db5387218a
Riley Mack <riley.j.mack@gmail.com>
2025-02-21 10:59:47 -0500

    first commit

---
+++ app/projects/page.tsx
@@ -0,0 +1,18 @@
+import { BlogPosts } from "app/components/posts";
+
+export const metadata = {
+  title: "Portfolio",
+  description: "Riley Macks Portfolio.",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        My Portfolio
+      </h1>
+
+      <BlogPosts />
+    </section>
+  );
+}
